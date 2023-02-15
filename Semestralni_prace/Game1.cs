38bcf95436ae20690be85cf75a6338bf088c6ee9
Prokop Mikulasek <prokop12@volny.cz>
2023-02-16 00:44:23 +0100

    Funguje vykreslovani kulek, ale nehybou se

--- Semestralni_prace/Game1.cs
+++ Semestralni_prace/Game1.cs
@@ -76,7 +76,6 @@ namespace Semestralni_prace
             this.bef = new BaseEnemyFactory(this);
             this.aef = new AdvancedEnemyFactory(this);
             this.bref = new BruteEnemyFactory(this);
-            this.bf = new BulletFactory(this, this.gameTime, new BulletFlyweight(this._content, this, this.bulletTexture, 1, 7));
 
             base.Initialize();
         }
@@ -100,6 +99,12 @@ namespace Semestralni_prace
 
         protected override void Update(GameTime gameTime)
         {
+            // tvorba bullet factory, pokud neexistuje
+            if (this.bf == null)
+            {
+                this.bf = new BulletFactory(this, this.gameTime, new BulletFlyweight(this._content, this, this.bulletTexture, 1, 7));
+            }
+            
             // konec Escapem
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
                 Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -254,7 +259,6 @@ namespace Semestralni_prace
             _spriteBatch.Begin();
             
             // vykresleni pozadi
-            /*
             for (int x = 0; x < GraphicsDevice.Viewport.Width; x += _backgroundTile.Width)
             {
                 for (int y = 0; y < GraphicsDevice.Viewport.Height; y += _backgroundTile.Height)
@@ -262,8 +266,7 @@ namespace Semestralni_prace
                     _spriteBatch.Draw(_backgroundTile, new Vector2(x, y), Color.White);
                 }
             }
-            */
-    
+
             // vykresleni hrace
             Player.Draw(_spriteBatch);
             
